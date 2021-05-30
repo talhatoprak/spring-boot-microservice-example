d49aab990f1f3c479f7f64ad3782de397602e876
Talha Toprak <toprak.urfa@gmail.com>
2021-05-30 18:20:03 +0300

    Initial commit

---
+++ spring-boot-microservice-product/spring-boot-microservice-product/src/main/java/com/talhatoprak/springbootmicroserviceproduct/SpringBootMicroserviceProductApplication.java
@@ -0,0 +1,13 @@
+package com.talhatoprak.springbootmicroserviceproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceProductApplication.class, args);
+	}
+
+}
